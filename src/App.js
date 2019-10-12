a2218686b403b6f77ea5751803825e8b318e0bfe
Luke Charles <lukeowaincharles@gmail.com>
2019-10-12 13:03:57 +0100

    Updated files to import new component and scss partial

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.scss';
 import PageLoader from './components/PageLoader';
 import Nav from './components/Nav';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
       <PageLoader />
-      <Nav />
+      <main>
+        <Nav />
+        <Hero />
+      </main>
     </div>
   );
 }
