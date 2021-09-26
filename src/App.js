3f08511477b379776641b6e28b83be33d4526f3a
Luke Charles <lukeowaincharles@gmail.com>
2021-09-26 20:56:56 +0100

    Amended the code to pass the props correctly to use the component instead of repeating

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contact from "./components/Archive/Contact";
 import Projects from "./components/Archive/Projects";
 import Header from "./components/Portfolio/Header/Header";
 import { Container, Row, Col } from "react-bootstrap";
-// import BrowserWindow from "./components/Portfolio/Browser/BrowserWindow";
+import BrowserWindow from "./components/Portfolio/Browser/BrowserWindow";
 
 function App() {
   return (
@@ -37,28 +37,21 @@ function App() {
             exact
             path="/archive"
             render={() => (
-              <div className={`${"browser-window browser-window--archive dark"}`}>
-                <div className="browser-window__bar">
-                  <span className="browser-window__button close"></span>
-                  <span className="browser-window__button minimise"></span>
-                  <span className="browser-window__button enlarge"></span>
+              <BrowserWindow classes="browser-window--archive">
+                <div className="archived">
+                  <PageLoader />
+                  <main>
+                    <Nav />
+                    <Hero />
+                    <Projects />
+                    <footer className="section-margin">
+                      <About />
+                      <Experience />
+                      <Contact />
+                    </footer>
+                  </main>
                 </div>
-                <div className="browser-window__content">
-                  <div className="archived">
-                    <PageLoader /> 
-                    <main>
-                      <Nav />
-                      <Hero />
-                      <Projects />
-                      <footer className="section-margin">
-                        <About />
-                        <Experience />
-                        <Contact />
-                      </footer>
-                    </main>
-                  </div>
-                </div>
-              </div>
+              </BrowserWindow>
             )}
           />
           <Route exact path="/:id" component={ProjectDetails} />
--- src/components/Portfolio/Browser/BrowserWindow.js
+++ src/components/Portfolio/Browser/BrowserWindow.js
@@ -1,21 +1,18 @@
 import React, { useState } from "react";
 
-function BrowserWindow(children) {
+function BrowserWindow({ children, classes }) {
   const [browserColor] = useState("dark");
 
   return (
-    <div className={`${"browser-window"} ${browserColor}`}>
+    <div className={`${"browser-window"} ${classes} ${browserColor}`}>
       <div className="browser-window__bar">
         <span className="browser-window__button close"></span>
         <span className="browser-window__button minimise"></span>
         <span className="browser-window__button enlarge"></span>
       </div>
-      <div className="browser-window__content">
-        {children}
-      </div>
+      <div className="browser-window__content">{children}</div>
     </div>
-  )
+  );
 }
 
 export default BrowserWindow;
-
