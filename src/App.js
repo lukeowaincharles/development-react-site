300ceb38a6c1982ebabc0ef4f90bc260251ca796
Luke Charles <lukeowaincharles@gmail.com>
2021-09-26 15:36:56 +0100

    Added router to project, updated the markup to use the router, moved current website into archive page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
-import React from 'react';
-import './App.scss';
-import PageLoader from './components/PageLoader';
-import Nav from './components/Nav';
-import Hero from './components/Hero';
-import About from './components/About';
-import Experience from './components/Experience';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import ProjectDetails from "./components/Portfolio/Project-details";
+import "./App.scss";
+import PageLoader from "./components/PageLoader";
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <PageLoader />
-      <main>
-        <Nav />
-        <Hero />
-        <Projects />
-        <footer className="section-margin">
-          <About />
-          <Experience />
-          <Contact />
-        </footer>
-      </main>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <div className="App">HomePage</div>}
+        />
+        <Route
+          exact
+          path="/archive"
+          render={() => (
+            <div className="App">
+              <PageLoader />
+              <main>
+                <Nav />
+                <Hero />
+                <Projects />
+                <footer className="section-margin">
+                  <About />
+                  <Experience />
+                  <Contact />
+                </footer>
+              </main>
+            </div>
+          )}
+        />
+        <Route exact path="/:id" component={ProjectDetails} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
