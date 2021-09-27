7032dfb7b309ba14eb6a5438cb2fa31fd8d2b432
Luke Charles <lukeowaincharles@gmail.com>
2021-09-27 08:01:03 +0100

    Updated the markup, data and added images for the projects

--- src/components/Portfolio/Projects/PortfolioProjects.js
+++ src/components/Portfolio/Projects/PortfolioProjects.js
@@ -1,16 +1,19 @@
 import React from "react";
 import BrowserWindow from "../Browser/BrowserWindow";
 import ProjectData from "../../../Data/Projects.json";
+import ButtonPrimary from "../Utilities/Button";
 
 function PortfolioProjects() {
   const CONTENT = ProjectData.map((item, index) => {
     return (
-      <React.Fragment>
+      <React.Fragment key={index}>
         {item.url && item.image ? (
           <BrowserWindow key={index} classes={item.url}>
             <div>
               <img src={ `${item.image}`} alt="" />
             </div>
+            <h2>{item.headline}</h2>
+            <ButtonPrimary link={item.url} title="View casestudy" />
           </BrowserWindow>
         ) : (
           ""
