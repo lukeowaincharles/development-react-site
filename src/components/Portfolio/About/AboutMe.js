586ca50943f6d7a072b48c7d8a2d6aad6d155158
Luke Charles <lukeowaincharles@gmail.com>
2021-09-28 20:13:59 +0100

    Updated the markup and Json file for about me sections

--- src/components/Portfolio/About/AboutMe.js
+++ src/components/Portfolio/About/AboutMe.js
@@ -5,12 +5,10 @@ import ButtonPrimary from "../Utilities/Button";
 
 function AboutMe() {
   const ABOUTCONTENT = AboutData.content,
-    TITLE = ABOUTCONTENT[0].title,
-    NAME = ABOUTCONTENT[0].name;
+    TITLE = ABOUTCONTENT[0].title;
   return (
-    <BrowserWindow classes="aboutme">
+    <BrowserWindow classes="aboutme" tab="About Luke">
       {TITLE ? <p>{TITLE}</p> : ""}
-      {NAME ? <p>{NAME}</p> : ""}
       {ABOUTCONTENT[0].image ? <div><img src={ `${ABOUTCONTENT[0].image}`} alt="" /></div> : ""}
       <ButtonPrimary link="about" title="Find out more" classes="purple" />
     </BrowserWindow>
