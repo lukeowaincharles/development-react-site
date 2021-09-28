33c3bdea10e44717245bfbfacc39e524b77973be
Luke Charles <lukeowaincharles@gmail.com>
2021-09-28 20:29:38 +0100

    Added markup to taskbar and added a links json file to populate the content

--- src/components/Portfolio/Taskbar/Taskbar.js
+++ src/components/Portfolio/Taskbar/Taskbar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import LinkData from "../../../Data/Links.json";
 
 function Taskbar() {
-
+  const LINKCONTENT = LinkData.map((item, index) => {
+    return (
+      <a
+        href={item.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="app-folder__link"
+      >
+        <div className="app-folder__content">
+          <div className="app-folder__background">
+            <div className={`${"app-folder__icon"} ${item.icon}`}></div>
+          </div>
+        </div>
+      </a>
+    );
+  });
+  return <div className="taskbar">{LINKCONTENT}</div>;
 }
 
 export default Taskbar;
