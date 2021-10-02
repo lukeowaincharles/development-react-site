08134ab95b9fabe73eb5e7ec01423ef3f8d5d013
Luke Charles <lukeowaincharles@gmail.com>
2021-10-02 10:50:17 +0100

    Added in the menu system for the taskbar, updated the styling to remove the overflow

--- src/components/Portfolio/Taskbar/Taskbar.js
+++ src/components/Portfolio/Taskbar/Taskbar.js
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import LinkData from "../../../Data/Links.json";
 
 function Taskbar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const MENUITEMS = [
+    { url: "silverstone", title: "Silverstone" },
+    { url: "jmfinn", title: "JMFinn" },
+    { url: "hiscox", title: "Hiscox" },
+    { url: "teenage-cancer-trust", title: "Teenage Cancer Trust" },
+    { url: "run-digital", title: "Run Digital" },
+  ];
+  const PROJECTMENUITEMS = MENUITEMS.map((item, index) => {
+    return (
+      <li>
+        <a href={item.url}>{item.title}</a>
+      </li>
+    );
+  });
+
   const LINKCONTENT = LinkData.map((item, index) => {
     return (
       <a
         href={item.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="app-folder__link"
+        target={item.target}
+        rel={item.target === "_blank" ? "noopener noreferrer" : "bookmark"}
+        className={`${"app-folder__link"} ${item.icon}`}
         key={index}
         aria-label={`${"Link to"} ${item.title}`}
       >
@@ -20,7 +37,28 @@ function Taskbar() {
       </a>
     );
   });
-  return <div className="taskbar">{LINKCONTENT}</div>;
+  return (
+    <div className="taskbar">
+      <div className="app-folder__link menu">
+        <div
+          className="app-folder__background"
+          onClick={() => {
+            setOpen(!isOpen);
+          }}
+        >
+          <div className="app-folder__icon"></div>
+        </div>
+        {isOpen ? (
+          <div className="project-menu">
+            <ul>{PROJECTMENUITEMS}</ul>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      {LINKCONTENT}
+    </div>
+  );
 }
 
 export default Taskbar;
