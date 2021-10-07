acf1ca918c50cff557b58e2b25ac30dbb6a1fd9a
Luke Charles <lukeowaincharles@gmail.com>
2021-10-07 15:19:49 +0100

    Refactor code to make it cleaner and easier to read - updated the objects, conditionals and the way the content is pulled through

--- src/components/Portfolio/Projects/ProjectDetails.js
+++ src/components/Portfolio/Projects/ProjectDetails.js
@@ -12,65 +12,29 @@ import Contact from "../Contact/Contact";
 function ProjectDetails() {
   const [id] = useState(useParams().id),
     [url, setUrl] = useState(""),
-    [logo, setLogo] = useState(""),
-    [headline, setHeadline] = useState(""),
-    [section_1_title, setSection1Title] = useState(""),
-    [section_1_text, setSection1Text] = useState(""),
-    [section_1_image, setSection1Image] = useState(""),
-    [live_site, setLiveSite] = useState(""),
-    [alias, setAlias] = useState(""),
-    [stack, setStack] = useState(""),
-    [section_2_text, setSection2Text] = useState(""),
-    [section_2_image, setSection2Image] = useState(""),
-    [section_3_text, setSection3Text] = useState(""),
-    [section_3_image, setSection3Image] = useState(""),
-    [intro, setIntro] = useState(""),
-    [description, setDescription] = useState(""),
-    [stack_skills, setStackSkills] = useState("");
+    [page, setPage] = useState(null),
+    [aboutPage, setAboutPage] = useState(null);
 
   useEffect(() => {
     for (let i = 0; i < ProjectData.length; i++) {
       let obj = ProjectData[i];
       if (obj.url === id) {
         setUrl(obj.url);
-        setLogo(obj.logo);
-        setHeadline(obj.headline);
-        setSection1Title(obj.section_1_title);
-        setSection1Text(obj.section_1_text);
-        setSection1Image(obj.section_1_image);
-        setLiveSite(obj.live_site);
-        setAlias(obj.alias);
-        setStack(obj.stack);
-        setSection2Text(obj.section_2_text);
-        setSection2Image(obj.section_2_image);
-        setSection3Text(obj.section_3_text);
-        setSection3Image(obj.section_3_image);
+        setPage(obj);
       }
     }
     for (let i = 0; i < SubProjectData.length; i++) {
       let obj = SubProjectData[i];
       if (obj.url === id) {
         setUrl(obj.url);
-        setHeadline(obj.headline);
-        setSection1Title(obj.section_1_title);
-        setSection1Text(obj.section_1_text);
-        setSection1Image(obj.section_1_image);
-        setLiveSite(obj.live_site);
-        setAlias(obj.alias);
-        setStack(obj.stack);
-        setSection2Text(obj.section_2_text);
-        setSection2Image(obj.section_2_image);
-        setSection3Text(obj.section_3_text);
-        setSection3Image(obj.section_3_image);
+        setPage(obj);
       }
     }
     for (let i = 0; i < AboutData.length; i++) {
       let obj = AboutData[i];
       if (obj.url === id) {
         setUrl(obj.url);
-        setIntro(obj.intro);
-        setDescription(obj.description);
-        setStackSkills(obj.stack_skills);
+        setAboutPage(obj);
       }
     }
   }, [id]);
@@ -135,21 +99,21 @@ function ProjectDetails() {
                 <div className={`${"project-details"} ${url}`} id={`${id}`}>
                   <div className="project-details__header">
                     <Container>
-                      {logo && (
+                      {page?.logo && (
                         <div className="projects__logo">
                           <img
                             width="100%"
                             height="100%"
-                            src={logo}
+                            src={page.logo}
                             alt={`${url} ${"logo"}`}
                           />
                         </div>
                       )}
-                      {headline && <h1>{headline}</h1>}
-                      {intro && (
+                      {page?.headline && <h1>{page.headline}</h1>}
+                      {aboutPage?.intro && (
                         <Row>
                           <Col lg={6}>
-                            <h1>{intro}</h1>
+                            <h1>{aboutPage.intro}</h1>
                           </Col>
                           <Col lg={6}>
                             <div className="about__image">
@@ -165,33 +129,37 @@ function ProjectDetails() {
                       <Container>
                         <Row>
                           <Col xl={6}>
-                            {section_1_title && <h2>{section_1_title}</h2>}
-                            {section_1_text && <p>{section_1_text}</p>}
-                            {live_site && (
+                            {page?.section_1_title && (
+                              <h2>{page.section_1_title}</h2>
+                            )}
+                            {page?.section_1_text && (
+                              <p>{page.section_1_text}</p>
+                            )}
+                            {page?.live_site && (
                               <div className="project-details__information">
                                 <div className="project-details__site">
                                   <h3>Live site</h3>
                                   <p>
                                     <a
-                                      href={live_site}
+                                      href={page.live_site}
                                       target="_blank"
                                       rel="noopener noreferrer"
                                     >
-                                      {alias}
+                                      {page.alias}
                                     </a>
                                   </p>
                                 </div>
                                 <div className="project-details__stack">
                                   <h3>Stack</h3>
-                                  <p>{stack}</p>
+                                  <p>{page.stack}</p>
                                 </div>
                               </div>
                             )}
-                            {description && (
+                            {aboutPage?.description && (
                               // ABOUT Desc
                               <div className="about__description">
                                 <h2>A bit about me</h2>
-                                {AboutData[0].description.map(
+                                {aboutPage.description.map(
                                   (paragraph, i) => (
                                     <p key={i}>{paragraph}</p>
                                   )
@@ -200,18 +168,18 @@ function ProjectDetails() {
                             )}
                           </Col>
                           <Col xl={6}>
-                            {section_1_image && (
+                            {page?.section_1_image && (
                               <div className="project-details__image project-details__image--one">
-                                <img src={section_1_image} alt="" />
+                                <img src={page.section_1_image} alt="" />
                               </div>
                             )}
-                            {stack_skills && (
+                            {aboutPage?.stack_skills && (
                               <div className="about__skills">
                                 <h3 className="about__skills__title">
                                   Stack and skills
                                 </h3>
                                 <ul>
-                                  {AboutData[0].stack_skills.map(
+                                  {aboutPage.stack_skills.map(
                                     (listItem, i) => (
                                       <li key={i}>{listItem}</li>
                                     )
@@ -247,17 +215,21 @@ function ProjectDetails() {
                         <Container>
                           <Row>
                             <Col xl={6}>
-                              {section_2_image && (
+                              {page?.section_2_image && (
                                 <div className="project-details__image project-details__image--two">
-                                  <img src={section_2_image} alt="" />
+                                  <img src={page.section_2_image} alt="" />
                                 </div>
                               )}
                             </Col>
                             <Col xl={6}>
-                              {section_2_text && (
+                              {page?.section_2_text && (
                                 <div className="project-details__approach">
-                                  <h2>What the approach of the project was</h2>
-                                  <p>{section_2_text}</p>
+                                  <h2>The approach</h2>
+                                  {page.section_2_text.map(
+                                    (paragraph, index) => (
+                                      <p key={index}>{paragraph}</p>
+                                    )
+                                  )}
                                 </div>
                               )}
                             </Col>
@@ -268,27 +240,58 @@ function ProjectDetails() {
                     {url === "about" ? (
                       ""
                     ) : (
-                      <div className="project-details__wrapper">
-                        <Container>
-                          <Row>
-                            <Col xl={6}>
-                              {section_3_text && (
-                                <div className="project-details__approach">
-                                  <h2>What the outcome was of the project</h2>
-                                  <p>{section_3_text}</p>
-                                </div>
-                              )}
-                            </Col>
-                            <Col xl={6}>
-                              {section_3_image && (
-                                <div className="project-details__image project-details__image--three">
-                                  <img src={section_3_image} alt="" />
-                                </div>
-                              )}
-                            </Col>
-                          </Row>
-                        </Container>
-                      </div>
+                      <>
+                        <div className="project-details__wrapper">
+                          <Container>
+                            <Row>
+                              <Col xl={6}>
+                                {page?.section_3_text && (
+                                  <div className="project-details__approach">
+                                    <h2>My role</h2>
+                                    {page.section_3_text.map(
+                                      (paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                      )
+                                    )}
+                                  </div>
+                                )}
+                              </Col>
+                              <Col xl={6}>
+                                {page?.section_3_image && (
+                                  <div className="project-details__image project-details__image--three">
+                                    <img src={page.section_3_image} alt="" />
+                                  </div>
+                                )}
+                              </Col>
+                            </Row>
+                          </Container>
+                        </div>
+                        <div className="project-details__wrapper">
+                          <Container>
+                            <Row>
+                              <Col xl={6}>
+                                {page?.section_4_image && (
+                                  <div className="project-details__image project-details__image--four">
+                                    <img src={page.section_4_image} alt="" />
+                                  </div>
+                                )}
+                              </Col>
+                              <Col xl={6}>
+                                {page?.section_4_text && (
+                                  <div className="project-details__approach">
+                                    <h2>The outcome</h2>
+                                    {page.section_4_text.map(
+                                      (paragraph, index) => (
+                                        <p key={index}>{paragraph}</p>
+                                      )
+                                    )}
+                                  </div>
+                                )}
+                              </Col>
+                            </Row>
+                          </Container>
+                        </div>
+                      </>
                     )}
                     <Contact />
                   </div>
